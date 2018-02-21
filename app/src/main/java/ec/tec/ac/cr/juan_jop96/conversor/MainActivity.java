efbbf8be03b4db8d33e2c165650707f816414005
Juan Jose A. Castro <juan_jop96@hotmail.com>
2018-02-20 20:17:52 -0600

    Funcional 1.0

---
+++ app/src/main/java/ec/tec/ac/cr/juan_jop96/conversor/MainActivity.java
@@ -0,0 +1,38 @@
+package ec.tec.ac.cr.juan_jop96.conversor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button BTConvertir = findViewById(R.id.BTConvertir);
+        final RadioButton DaC = findViewById(R.id.RDDaC);
+        RadioButton CaD = findViewById(R.id.RDCaD);
+        final EditText Monto = findViewById(R.id.ETMonto);
+        CaD.setChecked(true);
+        BTConvertir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String MtC = Monto.getText().toString();
+                float Pre = Float.parseFloat(MtC);
+                if (MtC.isEmpty())
+                    MtC = "0";
+                if (DaC.isChecked())
+                    Pre *= 569.32;
+                else
+                    Pre /= 569.32;
+                MtC = String.valueOf(Pre);
+                Toast.makeText(MainActivity.this, MtC, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
